4c5d17f5ec5966686f60015bd524a0b42f1fcfe1
Gowtham Siddharthan <gowthamsiddharthan88@gmail.com>
2024-12-27 22:50:26 +0530

    final-3

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+
 import Link from "next/link";
 
 
@@ -14,7 +14,7 @@ export default function Home() {
 
                   <figure className="relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
                     <span  >
-                      <img className=" rounded-lg  max-w-xl" style={{ height: "325px" }} src="https://www.tripplannersindia.com/assets/images/postimages/Aagaya_Gangai_Waterfalls.webp" alt="image description" />
+                      <img className=" rounded-lg  max-w-xl" style={{ height: "325px" }} src="https://www.tripplannersindia.com/assets/images/postimages/Aagaya_Gangai_Waterfalls.webp" alt="" />
                     </span>
                   </figure>
                 </div>
@@ -29,7 +29,7 @@ export default function Home() {
                 30 April 2024
               </div>
               <div className="prose lg:prose-lg leading-relaxed md:text-lg line-clamp-4 text-muted-foreground">
-                Heartbroken, I traveled to Kollihills seeking healing. Amid ancient Hindu traditions, majestic peaks, and remote villages, I shed layers of pain. Kollihill's spiritual energy and simplicity reignited my essence. Travel's power immersed me in new beliefs, allowing me to rediscover my true self.
+                Heartbroken, I traveled to Kollihills seeking healing. Amid ancient Hindu traditions, majestic peaks, and remote villages, I shed layers of pain. Kollihill&apos;s spiritual energy and simplicity reignited my essence. Travel&apos;s power immersed me in new beliefs, allowing me to rediscover my true self.
               </div>
               <div className="text-sm text-muted-foreground">
                 <div className="mr-2 inline-block">
@@ -46,7 +46,7 @@ export default function Home() {
                 <div className="col-start-1 col-span-12">
                   <figure className="relative max-w-lg transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
                     <span  >
-                      <img className="rounded-lg  max-w-xl" style={{ height: "325px" }} src="https://content.r9cdn.net/rimg/dimg/26/2e/ad960cee-city-60709-168cd171c21.jpg?width=1366&height=768&xhint=1370&yhint=964&crop=true&watermarkposition=lowerright" alt="image description" />
+                      <img className="rounded-lg  max-w-xl" style={{ height: "325px" }} src="https://content.r9cdn.net/rimg/dimg/26/2e/ad960cee-city-60709-168cd171c21.jpg?width=1366&height=768&xhint=1370&yhint=964&crop=true&watermarkposition=lowerright" alt="" />
                     </span>
                   </figure>
                 </div>
@@ -78,7 +78,7 @@ export default function Home() {
 
                   <figure className=" max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
                     <span  >
-                      <img className="rounded-lg max-w-xl" style={{ height: "325px" }} data-nimg="fill" src="https://www.indiautentica.com/wp-content/uploads/2022/10/29ezcwtmtnm.jpg" alt="image description" />
+                      <img className="rounded-lg max-w-xl" style={{ height: "325px" }} data-nimg="fill" src="https://www.indiautentica.com/wp-content/uploads/2022/10/29ezcwtmtnm.jpg" alt="" />
                     </span>
                   </figure>
                 </div>
@@ -87,13 +87,13 @@ export default function Home() {
         <div className="grid grid-cols-1 gap-3 md:col-span-2 mt-4">
           <h2 className="font-sans font-semibold tracking-tighter text-primary text-2xl md:text-3xl">
             <Link href="/post_3">
-            “God's Own Country”: The Green nature</Link>
+            &apos;&apos;God&apos;s Own Country&apos;&apos;: The Green nature</Link>
           </h2><br></br>
           <div className="prose lg:prose-lg italic tracking-tighter text-muted-foreground">
             30 April 2024
           </div>
           <div className="prose lg:prose-lg leading-relaxed md:text-lg line-clamp-4 text-muted-foreground">
-          Known for its serene backwaters, picturesque beaches, lush green forests and misty hills, Kerala is often referred to as “God's Own Country”. Kerala is a popular tourist destination for both domestic and international travelers. Kerala is southwest coastal area of India providing
+          Known for its serene backwaters, picturesque beaches, lush green forests and misty hills, Kerala is often referred to as &apos;&apos;God&apos;s Own Country&apos;&apos;. Kerala is a popular tourist destination for both domestic and international travelers. Kerala is southwest coastal area of India providing
           </div>
           <div className="text-sm text-muted-foreground">
             <div className="mr-2 inline-block">
@@ -111,7 +111,7 @@ export default function Home() {
 
             <figure className="relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
               <span  >
-                <img className="rounded-lg max-w-xl"  style={{ height: "325px" }} src="https://himalayanrider.wordpress.com/wp-content/uploads/2018/03/img_20170609_185809_hdr.jpg?w=672&h=372&crop=1" alt="image description" />
+                <img className="rounded-lg max-w-xl"  style={{ height: "325px" }} src="https://himalayanrider.wordpress.com/wp-content/uploads/2018/03/img_20170609_185809_hdr.jpg?w=672&h=372&crop=1" alt="" />
               </span>
             </figure>
             </div>
@@ -125,7 +125,7 @@ export default function Home() {
             30 April 2024
           </div>
           <div className="prose lg:prose-lg leading-relaxed md:text-lg line-clamp-4 text-muted-foreground">
-          Ladakh is most famous for breathtaking landscapes, the crystal clear skies, the highest mountain passes, thrilling adventure activities, Buddhist Monasteries and festivals. but it is also the place where you will find the Magnetic Hill, the one-of-a-kind phenomena that defies gravity.          </div>
+          Ladakh is most famous for breathtaking landscapes, the crystal clear skies, the highest mountain passes, thrilling adventure activities, Buddhist Monasteries and festivals. but it is also the place where you will find the Magnetic Hill, the one-of-a-kind phenomena that defies gravity.</div>
           <div className="text-sm text-muted-foreground">
             <div className="mr-2 inline-block">
               <span  >#Ladakh</span>
@@ -142,7 +142,7 @@ export default function Home() {
 
             <figure className="relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
               <span  >
-                <img className="rounded-lg max-w-xl"  style={{ height: "325px" }} src="https://www.shutterstock.com/image-photo/arial-view-auroville-experimental-township-600nw-1967101996.jpg" alt="image description" />
+                <img className="rounded-lg max-w-xl"  style={{ height: "325px" }} src="https://www.shutterstock.com/image-photo/arial-view-auroville-experimental-township-600nw-1967101996.jpg" alt="" />
               </span>
             </figure>
             </div>
@@ -150,13 +150,13 @@ export default function Home() {
         <div className="grid grid-cols-1 gap-3 md:col-span-2 mt-4">
           <h2 className="font-sans font-semibold tracking-tighter text-primary text-2xl md:text-3xl">
             <Link href="/post_5">
-            Union territory "real Paradise": French colony</Link>
+            Union territory &apos;real Paradise&apos;: French colony</Link>
           </h2><br></br>
           <div className="prose lg:prose-lg italic tracking-tighter text-muted-foreground">
             30 April 2024
           </div>
           <div className="prose lg:prose-lg leading-relaxed md:text-lg line-clamp-4 text-muted-foreground">
-          Amongst the many attractions that this charming town boasts of, the most outstanding is the French Colony, also called the White Town. Flaunting cobblestone streets and yellow houses, this old French residential area retains its old world charm, picturesqueness and also the architecture of the old French quarters.          </div>
+          Amongst the many attractions that this charming town boasts of, the most outstanding is the French Colony, also called the White Town. Flaunting cobblestone streets and yellow houses, this old French residential area retains its old world charm, picturesqueness and also the architecture of the old French quarters.</div>
           <div className="text-sm text-muted-foreground">
             <div className="mr-2 inline-block">
               <span  >#Pondicherry</span>
@@ -173,7 +173,7 @@ export default function Home() {
 
             <figure className="relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
               <span  >
-                <img className="rounded-lg max-w-xl"  style={{ height: "325px" }} src="https://www.cholaimpressions.com/Blog%20Images/Culture%20And%20Tourism/4193143492_ca23db0df4_o.jpg" alt="image description" />
+                <img className="rounded-lg max-w-xl"  style={{ height: "325px" }} src="https://www.cholaimpressions.com/Blog%20Images/Culture%20And%20Tourism/4193143492_ca23db0df4_o.jpg" alt="" />
               </span>
             </figure>
             </div>
@@ -181,13 +181,13 @@ export default function Home() {
         <div className="grid grid-cols-1 gap-3 md:col-span-2 mt-4">
           <h2 className="font-sans font-semibold tracking-tighter text-primary text-2xl md:text-3xl">
             <Link href="/post_6">
-              Land of culture and architecture: The chola's Kingdom</Link>
+              Land of culture and architecture: The chola&apos;s Kingdom</Link>
           </h2>
           <div className="prose lg:prose-lg italic tracking-tighter text-muted-foreground">
             30 April 2024
           </div>
           <div className="prose lg:prose-lg leading-relaxed md:text-lg line-clamp-4 text-muted-foreground">
-          The Brihadeshwara Temple at Thanjavur was built by Rajaraja I in 1009 CE for worshipping Lord Shiva. Thanjavur is called 'The Rice Bowl of Tamil Nadu' because of its agricultural activities in the delta region of river Cauvey. The temple, culture and architecture of Thanjavur are famous throughout world. </div>
+          The Brihadeshwara Temple at Thanjavur was built by Rajaraja I in 1009 CE for worshipping Lord Shiva. Thanjavur is called &apos;The Rice Bowl of Tamil Nadu&apos; because of its agricultural activities in the delta region of river Cauvey. The temple, culture and architecture of Thanjavur are famous throughout world. </div>
           <div className="text-sm text-muted-foreground">
             <div className="mr-2 inline-block">
               <span  >#Thanjavur</span>
