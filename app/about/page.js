daa3f2732658e6e30566bc775f25ad5b2ec1c339
Gowtham Siddharthan <gowthamsiddharthan88@gmail.com>
2024-12-27 18:27:31 +0530

    final-1

--- app/about/page.js
+++ app/about/page.js
@@ -3,14 +3,14 @@ export default function About() {
         <>
 
             <div className="container mx-auto px-5 mb-10"> 
-                <div div className="grid grid-cols-12 gap-4 ">       
+                <div className="grid grid-cols-12 gap-4 ">       
                     <div className="col-start-3 col-span-7 text-lg font-sans text-justify"> 
                         <h1 className="text-4xl">About Me</h1><br></br>
-                        <p><img className="max-w-max h-auto mx-auto" src="https://fast.image.delivery/dtqiphv.jpg" alt="Samantha" /></p><br></br>
+                        <p><img className="max-w-max h-auto mx-auto" src="https://fast.image.delivery/dtqiphv.jpg" alt="" /></p><br></br>
                         <p>Hey there! I'm Siddharth, a 27-year-old former corporate warrior who decided to ditch the 9-to-5 grind and embark on an adventure of a lifetime. After years of hustling in a high-pressure job, I realized that life is too short to be stuck in an office, staring at spreadsheets all day.</p><br>
                         </br>
                         <p>So I took a leap of faith, quit my cushy job in India, and decided to see the world on my own terms. No more stuffy meetings or rigid schedules – just me, my backpack, and an open road ahead.</p><br></br>
-                        <p ><img className="max-w-max h-auto mx-auto" src="https://media.istockphoto.com/id/1311078906/photo/portrait-of-handsome-young-man-wearing-sunglasses-and-white-tshirt-posing-on-city-street.jpg?s=612x612&w=0&k=20&c=VJnG1hCQ5TLLvRFyQpwzQd7kUFqgfmKaJ1g2E7oYmTA=" alt="Samantha" /></p><br></br>
+                        <p ><img className="max-w-max h-auto mx-auto" src="https://media.istockphoto.com/id/1311078906/photo/portrait-of-handsome-young-man-wearing-sunglasses-and-white-tshirt-posing-on-city-street.jpg?s=612x612&w=0&k=20&c=VJnG1hCQ5TLLvRFyQpwzQd7kUFqgfmKaJ1g2E7oYmTA=" alt="" /></p><br></br>
                         <p>This blog is where I'll be documenting my travels, sharing my experiences, and hopefully inspiring others to follow their wanderlust. From trekking through remote villages to savoring local cuisines, I'm on a mission to immerse myself in different cultures and create memories that will last a lifetime.</p><br></br>
                         <p>But this journey isn't just about checking off destinations from a bucket list. It's about self-discovery, personal growth, and finding the courage to live life on my own terms. I'll be honest and raw, sharing the highs and lows, the moments of pure bliss and the inevitable challenges that come with solo travel.</p><br></br>
                         <p>So join me on this adventure, and let's explore the world together! Who knows, maybe my stories will inspire you to take that leap of faith and pursue your own dreams, whatever they may be.</p><br></br>
